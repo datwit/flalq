5f93bc4de8dd8c713f53341c592b7812e455695f
Danay Meneses <danaymeneses@gmail.com>
2021-03-14 15:29:09 -0400

    Configuration of the variables to run the application, they are imported from the config.py file.

--- run.py
+++ run.py
@@ -12,4 +12,5 @@ application = create_app(app_config)
 
 # If we're running in stand alone mode, run the application
 if __name__ == "__main__":
-    application.run(host="localhost", port=5000)
+    application.run(host= app_config.HOST, port=app_config.PORT, debug=app_config.DEBUG)
+    # because when I not use 'debug=True' in run, even if app_config has 'DEBUG=TRUE', the application run on 'Debug mode: off'
