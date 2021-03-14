620455d22bc91bd64c0429b01f029381be446512
Danay Meneses <danaymeneses@gmail.com>
2021-03-14 15:21:11 -0400

    Configuration of environment variables and definition of variable 'app_config' to start the application.

--- api/config/config.py
+++ api/config/config.py
@@ -9,21 +9,18 @@ class Config(object):
     """Configure 'Config' base class with environment general variables"""
 
     DEBUG = True
-    # PORT = 5000
-    # HOST = "127.0.0.1"
+    TESTING = False
     SQLALCHEMY_ECHO = False
     # To Disable that a sign must be sended every time that an object is modified
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://<db_user>:<db_pass>@<db_host>:<port>/<db_name>"
-    # UPLOAD_FOLDER= '<upload_folder>'
-    # ALLOWED_EXTENSIONS = '<upload_file_extensions>'
-    # MAX_CONTENT_LENGTH = '<upload_top_file_size>'
-    TESTING = False
+    # # Disable CSRF protection in the testing configuration
+    # WTF_CSRF_ENABLED = False
+
+    PORT = 5000
+    HOST = "127.0.0.1"
     # SERVER_NAME = '127.0.0.1:5000'
     # PAGINATION_PAGE_SIZE = 5
     # PAGINATION_PAGE_ARGUMENT_NAME = 'page'
-    # # Disable CSRF protection in the testing configuration
-    # WTF_CSRF_ENABLED = False
 
 
 class ProductionConfig(Config):
@@ -42,7 +39,6 @@ class DevelopmentConfig(Config):
     """Configure 'DevelopmentConfig' class with 'Development' environment specific variables"""
 
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI')
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@127.0.0.1:3306/pruebadanay"
     SQLALCHEMY_ECHO = False
     UPLOAD_FOLDER= 'C:/Users/Danay/Desktop/danay_python/snippets/api_classic/api/images/'
